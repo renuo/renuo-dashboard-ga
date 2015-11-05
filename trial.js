759ecfce62c6ccfdbe29b8c95689aacd5c47745c
Josua Schmid <josua.schmid@renuo.ch>
2015-11-05 20:30:18 +0100

    try to call gapis (todo: add oauth)

--- trial.js
+++ trial.js
@@ -1,33 +1,38 @@
 var loopback = require("loopback");
  
 var ds = loopback.createDataSource({
-    connector: require("loopback-connector-rest"),
-    debug: false,
-    operations: [
-    {
-        template: {
+    "connector": require("loopback-connector-rest"),
+    "debug": true,
+    "operations": [{
+        "template": {
             "method": "GET",
-            "url": "http://maps.googleapis.com/maps/api/geocode/{format=json}",
+            "url": " https://www.googleapis.com/analytics/v3/data/ga",
             "headers": {
                 "accepts": "application/json",
-                "content-type": "application/json"
+                "content-type": "application/json",
+                "authorization": "Bearer {token}"
             },
             "query": {
-                "address": "{street},{city},{zipcode}",
-                "sensor": "{sensor=false}"
+                "ids": "ga:12345",
+                "startdate": "{startDate}",
+                "enddate": "{endDate}",
+                "metrics": "ga:sessions,ga:bounces"
             },
-            "responsePath": "$.results[0].geometry.location"
+            "responsePath": "$.totalResults"
         },
-        functions: {
-           "geocode": ["street", "city", "zipcode"]
+        "functions": {
+            "findByDateRange": ["token", "startDate", "endDate"]
         }
-    }
-]});
+    }]
+});
+
 
 var GaEndpoint = ds.createModel('ga-endpoint', {
 });
 
-GaEndpoint.geocode('107 S B St', 'San Mateo', '94401', function(err, gaEndpoint) {
+
+GaEndpoint.findByDateRange('token', '2015-08-01', '2015-08-30', function(err, gaEndpoint) {
     console.log(err);
     console.log(gaEndpoint);
 });
+
