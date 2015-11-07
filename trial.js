ad9c8d1db79c30843a36b68adafed0771ba61bbc
Josua Schmid <josua.schmid@renuo.ch>
2015-11-07 11:48:20 +0100

    added sample for ga api call

--- trial.js
+++ trial.js
@@ -1,5 +1,32 @@
 var loopback = require("loopback");
- 
+var google = require("googleapis");
+
+SERVICE_ACCOUNT_EMAIL = '';
+SERVICE_ACCOUNT_KEY_FILE = '';
+
+var authClient = new google.auth.JWT(
+    SERVICE_ACCOUNT_EMAIL,
+    SERVICE_ACCOUNT_KEY_FILE,
+    null, ['https://www.googleapis.com/auth/analytics.readonly']);
+
+authClient.authorize(function (err, tokens) {
+    console.log(err);
+    console.log(tokens);
+
+    var analytics = google.analytics('v3');
+    analytics.data.ga.get({
+        auth: authClient,
+        'ids': 'ga:',
+        'start-date': '7daysAgo',
+        'end-date': 'today',
+        'metrics': 'ga:sessions'
+    }, function(err, data) {
+        console.log(err);
+        console.log(data);
+    });
+});
+
+/*
 var ds = loopback.createDataSource({
     "connector": require("loopback-connector-rest"),
     "debug": true,
@@ -36,3 +63,4 @@ GaEndpoint.findByDateRange('token', '2015-08-01', '2015-08-30', function(err, ga
     console.log(gaEndpoint);
 });
 
+*/
